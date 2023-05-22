0a86784f17fbd5687ad574eb438e84fd7a5a7cf6
isssarapapSd <83002797+issarapapSd@users.noreply.github.com>
2023-05-22 23:37:36 +0700

    issarapapSd
    edit hamber ger

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,9 @@
 function toggleHam(x) {
     x.classList.toggle("change");
 
-    let myMenu = document.getElementById("myMenu");
+    let myMenu = document.getElementById('myMenu');
     if(myMenu.className === 'menu'){
-        myMenu.className += 'menu-active'
+        myMenu.className += ' menu-active'
     }
     else{
         myMenu.className = 'menu'
